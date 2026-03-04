76a3dd33c4dfc899d09b5df1cca7975507784682
Osvaldobrito <osvaldooabn@gmail.com>
2026-03-04 17:39:24 -0400

    Atualização do about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,108 +1,124 @@
+import line from "../../images/line.svg";
+import targetIcon from "../../images/target-icon.svg";
+import eyeIcon from "../../images/eye-icon.svg";
+import heartIcon from "../../images/heart-icon.svg";
+import arrowList from "../../images/arrow-list.svg";
+
 export default function About() {
   return (
     <section className="about" id="about">
-      <p className="about__me reveal" style={{ "--delay": "0ms" }}>
-        // sobre mim
-      </p>
-
-      <h2 className="about__title reveal" style={{ "--delay": "80ms" }}>
-        Quem sou <span>eu</span>
-      </h2>
+      <div className="services__start">
+        <img className="services__line reveal" src={line} />
+        <h2 className="services__title reveal">SOBRE NÓS</h2>
+        <img className="services__line reveal" src={line} />
+      </div>
 
       <div className="about__grid">
+        <h1 className="about__grid-title">
+          Há mais de 5 anos construindo sonhos
+        </h1>
         <p className="about__description reveal" style={{ "--delay": "160ms" }}>
-          Olá! Sou contador e desenvolvedor web. Minha jornada de programação
-          começou por gostar de tecnologias digitais e curiosidade.Busco sempre
-          entregar código limpo, de fácil entendimento e de fácil manutenção
-          assim como esse site que você está vendo.
+          A MP Engenharia nasceu da paixão por transformar projetos em
+          realidade. Com uma equipe experiente de engenheiros e arquitetos,
+          oferecemos soluções completas para construções residenciais e
+          comercias.
           <br />
           <br />
-          Quando não estou programando, estou procurando aprender novas
-          tecnologias na área de programação e buscando aprender cada vez mais
-          sobre novas tecnolgias e ferramentas que ajudem no desenvolvimento de
-          sites e aplicativos eficientes .
+          Nosso compromisso vai além da entrega: buscamos criar espaços que
+          melhorem a vida das pessoas, com segurança, qualidade e design único,
+          pensando em cada detalhe.
         </p>
+        <ul className="about__list">
+          <li className="about__list-elements">
+            <img
+              className="about__list-icon"
+              src={arrowList}
+              alt="Icone de Seta"
+            />
+            Equipe técnica altamente qualificada
+          </li>
+          <li className="about__list-elements">
+            <img
+              className="about__list-icon"
+              src={arrowList}
+              alt="Icone de Seta"
+            />
+            Materiais de primeira qualidade
+          </li>
+          <li className="about__list-elements">
+            <img
+              className="about__list-icon"
+              src={arrowList}
+              alt="Icone de Seta"
+            />
+            Garantia estendida em todas as obras
+          </li>
+          <li className="about__list-elements">
+            <img
+              className="about__list-icon"
+              src={arrowList}
+              alt="Icone de Seta"
+            />
+            Cumprimento rigoroso de prazos
+          </li>
+          <li className="about__list-elements">
+            <img
+              className="about__list-icon"
+              src={arrowList}
+              alt="Icone de Seta"
+            />
+            Acompanhamento em tempo real
+          </li>
+          <li className="about__list-elements">
+            <img
+              className="about__list-icon"
+              src={arrowList}
+              alt="Icone de Seta"
+            />
+            Atendimento personalizado
+          </li>
+        </ul>
 
         <div className="about__card reveal" style={{ "--delay": "240ms" }}>
-          <img className="about__icon" src="" alt="" />
-          <h3 className="card__title">Como eu penso?</h3>
-          <p className="card__subtitle">
-            Gosto de quebrar problemas grandes em partes pequenas e resolver uma
-            por uma.
+          <img className="about-card__icon" src={targetIcon} alt="" />
+          <h3 className="about-card__title">Missão</h3>
+          <p className="about-card__subtitle">
+            Entregar obras de excelência, dentro do prazo e orçamento, superando
+            as expectativas dos nossos clientes.
           </p>
         </div>
 
         <div className="about__card reveal" style={{ "--delay": "320ms" }}>
-          <img className="about__icon" src="" alt="" />
-          <h3 className="card__title">Como eu trabalho?</h3>
-          <p className="card__subtitle">
-            Clean Code, componentização, reaproveitamento e atenção a UX.
+          <img className="about-card__icon" src={eyeIcon} alt="" />
+          <h3 className="about-card__title">Visão</h3>
+          <p className="about-card__subtitle">
+            Ser referência em construção civil, reconhecida pela qualidade,
+            inovação e compromisso com resultados.
           </p>
         </div>
 
         <div className="about__card reveal" style={{ "--delay": "320ms" }}>
-          <img className="about__icon" src="" alt="" />
-          <h3 className="card__title">Como eu trabalho?</h3>
-          <p className="card__subtitle">
-            Clean Code, componentização, reaproveitamento e atenção a UX.
+          <img className="about-card__icon" src={heartIcon} alt="" />
+          <h3 className="about-card__title">Valores</h3>
+          <p className="about-card__subtitle">
+            Ética, transparência, comprometimento, qualidade e respeito ao
+            cliente e ao meio ambiente.
           </p>
         </div>
       </div>
 
-      <h3 className="about__tech reveal" style={{ "--delay": "120ms" }}>
-        Tecnologias Utilizadas
-      </h3>
-
-      <div
-        className="about__skills-wrapper reveal"
-        style={{ "--delay": "200ms" }}
-      >
-        <div className="about__skills-marquee">
-          <div className="about__skills-track">
-            <div className="about__skill">
-              <p>HTML</p>
-            </div>
-            <div className="about__skill">
-              <p>CSS</p>
-            </div>
-            <div className="about__skill">
-              <p>JavaScript</p>
-            </div>
-            <div className="about__skill">
-              <p>React</p>
-            </div>
-            <div className="about__skill">
-              <p>Node.js</p>
-            </div>
-            <div className="about__skill">
-              <p>Express</p>
-            </div>
-            <div className="about__skill">
-              <p>Git</p>
-            </div>
-
-            <div className="about__skill">
-              <p>HTML</p>
-            </div>
-            <div className="about__skill">
-              <p>CSS</p>
-            </div>
-            <div className="about__skill">
-              <p>JavaScript</p>
-            </div>
-            <div className="about__skill">
-              <p>React</p>
-            </div>
-            <div className="about__skill">
-              <p>Node.js</p>
-            </div>
-            <div className="about__skill">
-              <p>Express</p>
-            </div>
-            <div className="about__skill">
-              <p>Git</p>
-            </div>
-          </div>
+      <div className="about__informations">
+        <div className="informations">
+          <p className="informations-number">+5</p>
+          <p className="informations-text">anos de mercado</p>
+        </div>
+        <div className="informations">
+          <p className="informations-number">+100</p>
+          <p className="informations-text">projetos entregues</p>
+        </div>
+        <div className="informations">
+          <p className="informations-number">+20</p>
+          <p className="informations-text">profissionais</p>
         </div>
       </div>
     </section>
